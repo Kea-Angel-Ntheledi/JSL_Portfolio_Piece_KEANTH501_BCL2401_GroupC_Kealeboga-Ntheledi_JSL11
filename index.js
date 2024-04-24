27313bebdba1f7164809572efe9ff3120ed9d19b
Kea-Angel-Ntheledi <kealebogaangelntheledi@gmail.com>
2024-04-24 15:55:59 +0200

    added code for cancelling adding new task

--- index.js
+++ index.js
@@ -194,8 +194,12 @@ function setupEventListeners() {
   // Cancel editing task event listener
   const cancelEditBtn = document.getElementById("cancel-edit-btn");
   cancelEditBtn.onclick = () => toggleModal(false, elements.editTaskModal);
-  // Cancel adding new task event listener
+   // Cancel adding new task event listener
   const cancelAddTaskBtn = document.getElementById("cancel-add-task-btn");
+  cancelAddTaskBtn.addEventListener("click", () => {
+    toggleModal(false);
+    elements.filterDiv.style.display = "none"; // Also hide the filter overlay
+  });
   // Set up a click event listener to handle canceling the modal
   cancelAddTaskBtn.addEventListener("click", () => {
     toggleModal(false); //hides the modal
