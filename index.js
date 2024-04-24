a661b444a6a894407945eb264e73a8ea54fdd33d
Kea-Angel-Ntheledi <kealebogaangelntheledi@gmail.com>
2024-04-24 15:40:04 +0200

    fixed the event listener on creating different boards in the DOM

--- index.js
+++ index.js
@@ -91,16 +91,18 @@ function displayBoards(boards) {
     boardElement.textContent = board;
     boardElement.classList.add("board-btn");
 
-    boardElement.addEventListener("click", () => {
+    boardElement.onclick = () => {
       elements.headerBoardName.textContent = board;
       filterAndDisplayTasksByBoard(board);
       activeBoard = board; //assigns active board
       localStorage.setItem("activeBoard", JSON.stringify(activeBoard));
       styleActiveBoard(activeBoard);
-    });
+    };
     boardsContainer.appendChild(boardElement);
   });
 }
+
+
 
 // Filters tasks corresponding to the board name and displays them on the DOM.
 // TASK: Fix Bugs
